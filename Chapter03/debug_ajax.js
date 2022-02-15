c558bd85d933663c6a06de4f98563494dbcd36d1
Sammy <98495588+notfogel@users.noreply.github.com>
2022-02-15 12:39:46 -0600

    activity 4!

--- Chapter03/debug_ajax.js
+++ Chapter03/debug_ajax.js
@@ -1,20 +1,34 @@
 function debugCallback(response){
-	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(mydata))
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
 };
-
-function debugAjax(){
+function afcAjax(){
 	
-	var mydata;
-	
-	fetch("data/MegaCities.geojson")
+	//step1: use Fetch default method to retrieve data
+	fetch('data/MegaCities.geojson')
 		.then(function(response){
 			return response.json();
-		})
-		.then(function(response){
-			debugCallback(mydata);
-		})
+		}) //step2 (above) convert the json data to usable data 
+		.then(callback) //step3 send this newly usable data to the callback fxn
+		
 
-	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br>' + JSON.stringify(mydata))
+};	
+//defining the callback fxn (below)
+function callback(response){
+	var myData = response;
+	//pass the data pon the function side (Gareth, if you're reading this, pls go google "pass the kouchie by mighty diamonds"))
+	nextFunction(myData);
+	//console.log(JSON.stringify(response));
+};	
+function nextFunction(data){
+	console.log(JSON.stringify(data)); //contains response held by myData in callbaq
+	//this above console.log spits out the same info as the one in line 19, it just lacks the stringify part that makes it legible
+	//now im gonna pass this to a final fxn which publishes it to the div
+	finalFunction(data);
+}
+//this final fxn knows holds on to this variable containing the data and plops it into the div
+function finalFunction(data4div){
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br>' + JSON.stringify(data4div))
+
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(data4div))
 };
-
-document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(mydata))
+document.addEventListener('DOMContentLoaded',afcAjax);
---
+++ Chapter03/main.js
@@ -0,0 +1,155 @@
+//initialize function 
+function initialize(){
+	cities();
+	console.log('is this initialize fxn running?');
+};
+//need a function to create the cityPop array then create a table populated by it
+function cities(){
+//so right here is the array which has all the info we want for the table
+	var cityPop = [
+		{
+			city: 'Madison',
+			population: 233209
+		},
+		{
+			city: 'Milwaukee',
+			population: 594833
+		},
+		{
+			city: 'Green Bay',
+			population: 104057
+		},
+		{
+			city: 'Superior',
+			population: 27244
+		}
+	];
+	//below here is gonna be the table creation 
+	var table = document.createElement("table");
+	//now we're gonna create a header row and append it to our table
+	var headerRow = document.createElement("tr");
+	table.appendChild(headerRow);
+	//creates headers for "City" and "Population"
+	headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>");
+	//going to try out a loop here which should populate the body of the table w the array's content
+	cityPop.forEach(function(cityObject){
+		var rowHtml = "<tr><td>" + cityObject.city + "</td><td>" + cityObject.population + "</td></tr>";
+		table.insertAdjacentHTML('beforeend',rowHtml);
+	});
+	//now let's append that table to the div, shall we?
+	document.querySelector('#mydiv').appendChild(table);
+	//now let's call the fxns from below to add columns and interactivity to this here table!
+	addColumns(cityPop);
+	addEvents();
+};
+function addColumns(cityPop){
+	console.log('is this add columns fxn runnin?');
+	//selects all cells and creates rows as a new var from this selection
+	var rows = document.querySelectorAll('tr');
+	document.querySelectorAll("tr").forEach(function(row, i){
+
+    	if (i == 0){
+			//adds "City Size" as an adjacent header
+    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+    	} else {
+			//this else block assigns a size to a city once the if block is through assigning them headers
+    		var citySize;
+
+    			if (cityPop[i-1].population < 100000){
+    				citySize = 'Small';
+
+    			} else if (cityPop[i-1].population < 500000){
+    				citySize = 'Medium';
+
+    			} else {
+    				citySize = 'Large';
+    			};
+
+			//creates the "City Size" header
+			row.insertAdjacentHTML = '<td' + citySize + '</td>';
+			//time to append the city size designations to the table
+			var newRow = document.createElement('td')
+				newRow.innerHTML = citySize
+				row.appendChild(newRow);
+    	};
+    });
+};
+function addEvents(){
+
+	//before I can do anything with the table, we need to select it (below)
+	table = document.querySelector('table');
+	//now that it's selected, we can initiate the mouseover function
+	document.querySelector("table").addEventListener("mouseover", function(){
+		//below, a var called "color" invokes css to bring up the rgb palatte
+		var color = "rgb(";
+		//loop below generates a random color
+		for (var i=0; i<3; i++){
+			console.log('is this loop running?');//self explanatory console.log statement
+			//below, a var called "random" is created to generate a random number each time it's called
+			var random = Math.round(Math.random() * 255);
+			//this statement below makes it so that each time color is called, it picks whatever color equates to the randomly generated number
+			color += random;
+			//this if statement keeps the loop running as long as i is less than 2
+			if (i<2){
+				color += ",";
+			//the else closes the loop
+			} else {
+				color += ")";
+		
+			};
+		};
+		//this statement sets the value of the table's style to the color var
+		table.style.color = color;
+	});
+	//this little function below causes a click to prompt a message
+	function clickme(){
+
+		alert('Hey, you clicked me!');
+	};
+	//this statement prompts the clickme fxn to run every time a click occurs
+	document.querySelector("table").addEventListener("click", clickme)
+};
+//second task below!!!!!!!!!!!
+function debugCallback(response){
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
+};
+function afcAjax(){
+	
+	//step1: use Fetch default method to retrieve data
+	fetch('data/MegaCities.geojson')
+		.then(function(response){
+			return response.json();
+		}) //step2 (above) convert the json data to usable data 
+		.then(callback) //step3 send this newly usable data to the callback fxn
+		
+
+};	
+//defining the callback fxn (below)
+function callback(response){
+	var myData = response;
+	//pass the data pon the function side (Gareth, if you're reading this, pls go google "pass the kouchie by mighty diamonds"))
+	nextFunction(myData);
+	//console.log(JSON.stringify(response));
+};	
+function nextFunction(data){
+	console.log(JSON.stringify(data)); //contains response held by myData in callbaq
+	//this above console.log spits out the same info as the one in line 19, it just lacks the stringify part that makes it legible
+	//now im gonna pass this to a final fxn which publishes it to the div
+	finalFunction(data);
+}
+//this final fxn knows holds on to this variable containing the data and plops it into the div
+function finalFunction(data4div){
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br>' + JSON.stringify(data4div))
+
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(data4div))
+};
+
+//dual initialize!!!!
+document.addEventListener('DOMContentLoaded',initialize)
+
+document.addEventListener('DOMContentLoaded',afcAjax);
+
+
+
+
+
